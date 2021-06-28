4ab7c4b36962bbdd594b81a3658b1d4e9edd2d82
seohyunsim <ssh4563@naver.com>
2021-06-28 16:52:14 +0900

    COVID19 그래프 구현

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -4,25 +4,84 @@ import axios from 'axios'
 
 const Contents = () => {
 
-    const [confrimedData, setconfrimedData] = useState({
-        labels: ["1월", "2월", "3월"],
-        datasets: [
-            {
-                label: "국내 누적 확진자",
-                backgroundColor: "salmon",
-                fill: true,
-                data: [10, 5, 3]
-            },
-        ]
-    })
+    const [confrimedData, setconfrimedData] = useState({});
+    const [quarantinedData, setQuarantinedData] = useState({});
+    const [comparedData, setComparedData] = useState({});
 
     useEffect(() => {
         const fetchEvents = async () => {
             const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
-    
+            makeData(res.data);
         }
+        const makeData = (items) => {
+            const arr = items.reduce((acc, cur) => {
+                const currentDate = new Date(cur.Date);
+                const year = currentDate.getFullYear();
+                const month = currentDate.getMonth();
+                const date = currentDate.getDate();
+                const confirmed = cur.Confirmed;
+                const active = cur.Active;
+                const death = cur.Deaths;
+                const recorvered = cur.Recovered;
+                
+                const findItem = acc.find(a => a.year === year && a.month === month);
+
+                if (!findItem) {
+                    acc.push({year, month, date, confirmed, active, death, recorvered});
+                }
+                if (findItem && findItem.date < date) {
+                    findItem.active = active;
+                    findItem.death = death;
+                    findItem.date = date;
+                    findItem.year = year;
+                    findItem.month = month;
+                    findItem.recorvered = recorvered;
+                    findItem.confirmed = confirmed;
+                }
+
+                return acc;
+            }, []);
+            
+            const labels = arr.map(a => `${a.month + 1}월`);
+            setconfrimedData({
+                labels: labels,
+                datasets: [
+                    {
+                        label: "국내 누적 확진자",
+                        backgroundColor: "salmon",
+                        fill: true,
+                        data: arr.map(a => a.confirmed)
+                    },
+                ]
+            });
+            setQuarantinedData({
+                labels: labels,
+                datasets: [
+                    {
+                        label: "월별 격리자 현황",
+                        borderColor: "green",
+                        fill: false,
+                        data: arr.map(a => a.active)
+                    },
+                ]
+            });
+            const last = arr[arr.length -1]
+            setComparedData({
+                labels: ["확진자", "격리해제", "사망"],
+                datasets: [
+                    {
+                        label: "누적 확진, 해제, 사망 비율",
+                        backgroundColor: ["#ff3d67", '#059bff', '#ffc233'],
+                        borderColor: ["#ff3d67", '#059bff', '#ffc233'],
+                        fill: false,
+                        data: [last.confirmed, last.recorvered, last.death]
+                    },
+                ]
+            });
+        }
+
         fetchEvents();
-    })
+    }, [])
 
     return (
         <div>
@@ -32,6 +91,18 @@ const Contents = () => {
                     <div>
                         <Bar data={confrimedData} options={
                             {title: {display: true, text: "누적 확진자 추이", fontSize: 16}},
+                            {legend: {display: true, position: "bottom"}}
+                        }/>
+                    </div>
+                    <div>
+                        <Line data={quarantinedData} options={
+                            {title: {display: true, text: "월별 격리자 현황", fontSize: 16}},
+                            {legend: {display: true, position: "bottom"}}
+                        }/>
+                    </div>
+                    <div>
+                        <Doughnut data={comparedData} options={
+                            {title: {display: true, text: `누적 확진, 해제, 사망 (${new Date().getMonth() + 1}월)`, fontSize: 16}},
                             {legend: {display: true, position: "bottom"}}
                         }/>
                     </div>
